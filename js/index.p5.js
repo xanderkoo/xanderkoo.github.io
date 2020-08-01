8e505ebb6555d022a9f8d1265f5454f21e55b8be
xanderkoo <xanderkoo@gmail.com>
2020-08-01 00:26:04 -0700

    first commit

---
+++ js/index.p5.js
@@ -0,0 +1,36 @@
+p5.disableFriendlyErrors = true;
+
+var inc = 0.03;
+var start = 0;
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    updateAnimation();
+}
+
+function mouseMoved() {
+    updateAnimation();
+}
+
+function updateAnimation() {
+    background(255)
+    noFill();
+
+    for (var l = 0, offsetY = -height; l < 10; l++, offsetY += height/5) {
+        var offsetX = start;
+        
+        beginShape();
+    
+        for (var x = 0; x < width; x += 3) {
+            stroke(0);
+            // var y = map(noise(offsetX), 0, 1, 0, height);
+            var y = noise(offsetX) * height + offsetY;
+            vertex(x, y);
+    
+            offsetX += inc;
+        }
+        endShape();
+    }
+
+    start += 0.01;
+}
