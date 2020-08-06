b5cb08d90ef25be7c480903b804da81411a0d792
xanderkoo <xanderkoo@gmail.com>
2020-08-05 20:48:44 -0700

    better buttons

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
-var clickedButton = [];
-
-function buttonClicked(elt) {
-    clickedButton = elt;
+function mouseOverButton(elt) {
+    // clickedButton = elt;
+    moving=true
     elt.style.backgroundColor = "transparent";
     // elt.getElementById
 }
 
-function resetButtons() {
-    clickedButton.style.backgroundColor = "white";
+function mouseOutButton(elt) {
+    // clickedButton = elt;
+    moving=false
+    elt.style.backgroundColor = "white";
+    // elt.getElementById
 }
 
