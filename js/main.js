861d9ec47b6edf321d191a4e9f3da2fc82d469c3
xanderkoo <xanderkoo@gmail.com>
2020-12-02 20:13:16 -0800

    flattened layout into one page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
+var headshot = null;
+var headshotsDir = "./images/me/";
+var headshotNames = ["mask.png", "wg.png"];
+
+function locationHashChanged() {
+    let contentTitles = document.getElementsByClassName("title");
+    let contentTexts = document.getElementsByClassName("content");
+    for (let i = 0; i < contentTitles.length; i++) {
+        contentTitles[i].hidden = true;
+        contentTexts[i].hidden = true;
+    }
+
+    if (location.hash === "" || location.hash === "#home") {
+        document.querySelector(":root").style.setProperty("--textbox-width", "calc(var(--main-container-width) - var(--menubar-width))");
+        document.getElementById("home-title").hidden = false;
+        document.getElementById("home-content").hidden = false;
+        document.getElementsByClassName("menubar right")[0].hidden = true;
+    } else if (location.hash === "#aboutme") {
+        console.log("here");
+        document.querySelector(":root").style.setProperty("--textbox-width", "calc(var(--main-container-width) - 2 * var(--menubar-width))");
+        document.getElementById("aboutme-title").hidden = false;
+        document.getElementById("aboutme-content").hidden = false;
+        document.getElementsByClassName("menubar right")[0].hidden = false;
+        headshot = document.getElementById("headshot");
+        headshot.src = headshotsDir + headshotNames[Math.floor(Math.random() * headshotNames.length)];
+    }
+}
+  
+window.onhashchange = locationHashChanged;
+
+document.onreadystatechange = function(e)
+{
+    if (document.readyState === 'complete')
+    {
+        locationHashChanged();
+    }
+};
+
 async function mouseOverButton(elt) {
     scrolling = true;
     elt.style.cssText += ";border: var(--dashed-line-thickness) solid black; background-color: transparent"
